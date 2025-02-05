666d3cbed2455ac366a4e443d654e16ea704fdcd
pear_c <zxcv9676@naver.com>
2025-02-05 18:08:50 +0900

    스프링부트 - 게시판 (Create)

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form);
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        return "";
+    }
+}
