e90323d9d7e15cfc4500f48454e590d1a48e3da1
pear_c <zxcv9676@naver.com>
2025-02-10 15:51:44 +0900

    스프링부트 - 롬복 리팩토링

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
@@ -20,13 +22,16 @@ public class MemberController {
 
     @PostMapping("/join")
     public String createMember(MemberForm memberForm) {
+        log.info(memberForm.toString());
+
         // 1. DTO를 엔티티로 변환
         Member member = memberForm.toEntity();
-        System.out.println(member);
+        log.info(member.toString());
 
         // 2. 리파지터리로 엔티티를 DB에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved);
+        log.info(saved.toString());
+
         return "";
     }
 
