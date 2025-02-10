d08982fb2bc74d810cbe599bdd2e7b55d74e81e8
pear_c <zxcv9676@naver.com>
2025-02-10 16:19:45 +0900

    스프링부트 - 게시판 리다이렉트 설정

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -36,7 +36,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
 
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
 
     @GetMapping("/articles/{id}")   // 데이터 조회 요청 접수
