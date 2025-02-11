5ff1cf4067e9097b9ec8fce5d0bcada887c7a04e
pear_c <zxcv9676@naver.com>
2025-02-11 17:36:11 +0900

    스프링부트 - 트랜잭션, 롤백 테스트

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -17,6 +17,8 @@ import java.util.List;
 public class ArticleApiController {
 //    @Autowired
 //    private ArticleRepository articleRepository;
+    @Autowired
+    private ArticleService articleService;
 
     // GET
     @GetMapping("/api/articles")
@@ -56,7 +58,11 @@ public class ArticleApiController {
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
-    @Autowired
-    private ArticleService articleService;
-
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos) {
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -64,5 +66,22 @@ public class ArticleService {
         // 3. 대상 삭제하기
         articleRepository.delete(target);
         return target;
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // 1. dto 묶음을 엔티티 묶음으로 변환하기
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+
+        // 2. 엔티티 묶음을 DB에 저장하기
+        articleList.stream().forEach(article -> articleRepository.save(article));
+
+        // 3. 강제 예외 발생시키기
+        articleRepository.findById(-1L).orElseThrow(() -> new IllegalArgumentException("결제 실패!"));
+
+        // 4. 결과 값 반환하기
+        return articleList;
     }
 }
